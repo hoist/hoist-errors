c65362b6801f823593e80da483a0c93d12240193
Owen Evans <owen@bgeek.net>
2014-09-22 16:48:08 +1200

    inital commit

---
+++ tests/unit_tests/model/applicaiton_test.js
@@ -0,0 +1,19 @@
+'use strict';
+var errors = require('../../../lib');
+var expect = require('chai').expect;
+describe('model', function () {
+  describe('application', function () {
+    describe('NotFoundError', function () {
+      var notFoundError = new errors.model.application.NotFoundError();
+      it('should have a 404 code', function () {
+        expect(notFoundError.code).to.eql(404);
+      });
+      it('should be an error type', function () {
+        expect(notFoundError).to.be.instanceof(Error);
+      });
+      it('should be a model not found error', function () {
+        expect(notFoundError).to.be.instanceof(errors.model.NotFoundError);
+      });
+    });
+  });
+});
